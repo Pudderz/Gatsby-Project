3e620a0b99eaceb7abd9ac98449a622b9aee0839
Pudderz <mpudney6@gmail.com>
2020-10-15 21:52:01 +0100

    Fixed issue causing break in program when querying mdx when creating table of content
    This was due to the program looping through the nested array of headers using recursion without testing if there was a header present to style in the table of content when/before to comparing it to the current intersecting id of a header. This would break e.g when there was a h3 tag but no h2 tag above it in the nested array of id headers as it would try to slice an undefined value instead of the usual string.

--- src/Components/TableOfContent.js
+++ src/Components/TableOfContent.js
@@ -1,17 +1,18 @@
 import React, {useState, useEffect} from 'react'
 import './tocStyles.scss'
 function renderItems(items, activeId) {
-    console.log(items);
-    console.log(activeId);
     return (
       <ol>
         {items.map((item) => (
           <li key={item.url}>
-            <a href={item.url}
+              {item.url && 
+              <a href={item.url}
             style={{
                 color: activeId === item.url.slice(1) ? "white" : "grey",
               }}
             >{item.title}</a>
+              }
+            
             {item.items && renderItems(item.items, activeId)}
           </li>
         ))}
@@ -21,10 +22,8 @@ function renderItems(items, activeId) {
   
   function getIds(items) {
     return items.reduce((acc, item) => {
-        console.log(item);
       if (item.url) {
-          console.log(item.url)
-          console.log(item.url.slice(1))
+          
         // url has a # as first character, remove it to get just the id
         acc.push(item.url.slice(1));
       }
@@ -37,7 +36,7 @@ function renderItems(items, activeId) {
 
 
   function useActiveId(idArray) {
-    const [intersectingId, setIntersectingId] = useState();
+    const [intersectingId, setIntersectingId] = useState('placeholder');
 
     useEffect(() => {
       const observer = new IntersectionObserver(
@@ -69,17 +68,12 @@ function renderItems(items, activeId) {
 export function TableOfContents(props) {
     const idArray = getIds(props.items);
     const activeId = useActiveId(idArray);
-    console.log(activeId)
     return (
       <details>
         <summary>Table of Contents</summary>
         <ol>
             {renderItems(props.items, activeId)}
-          {/* {props.items.map((item) => (
-            <li key={item.url}>
-              <a href={item.url}>{item.title}</a>
-            </li>
-          ))} */}
+          
         </ol>
       </details>
     );
